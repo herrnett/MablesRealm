d99678ada87f09a0b2af426973dc4540788ced3c
unknown <>
2024-04-13 00:51:52 +0200

    even more setup

--- game.gd
+++ game.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Musicplayer.stream = AudioStreamOggVorbis.load_from_file("res://ost/LD48_alle_Ebenen.ogg")
+	# Musicplayer.stream = AudioStreamOggVorbis.load_from_file("res://ost/LD48_alle_Ebenen.ogg")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
